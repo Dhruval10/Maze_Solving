0c19503119f34d2c44201e3a1ec1c5d9f33e6de9
Dhruval <shahdhruval10@gmail.com>
2019-12-01 21:30:43 -0800

    Add files via upload

--- goTest.pl
+++ goTest.pl
@@ -1,13 +1,25 @@
-:- initialization([ 'logic.pl', 'publicRecursion1.pl', 'publicRecursion2.pl', 'publicMaze1.pl', 'publicMaze2.pl', 'publicEval.pl', 'publicVarsOf.pl', 'publicSat.pl']).
+:- initialization(
+    [ 'logic.pl'
+    , 'publicRecursion1.pl'
+    , 'publicRecursion2.pl'
+    , 'publicMaze1.pl'
+    , 'publicMaze2.pl'
+    ]).
 
 run_f(Func) :-
-     name(Func,FunName),
-     writef("Testcase: %s\n",[FunName]),
+    % name(Func,FunName),
+    % writef("Testcase: %s\n",[FunName]),
     Func.
-testcases([ public_prod, public_fill, public_genN, public_genXY, public_flat, public_stats, public_validPath, public_findDistance, public_solve
-    , public_eval
-    , public_varsOf
-    , public_sat
+
+testcases(
+    [ public_prod
+    , public_fill
+    , public_genN
+    , public_genXY
+    , public_flat
+    , public_stats
+    , public_validPath
+    , public_findDistance
     ]).
 
 run :-
